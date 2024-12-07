010722ba37d54f93223392de86ae75435d5f7e3d
Brent Gardner <bgardner@squarelabs.net>
2024-12-07 13:06:17 -0700

    correct movement

--- src/reducers/ghost.rs
+++ src/reducers/ghost.rs
@@ -12,42 +12,40 @@ pub fn ghosts(state: &GameState) -> (u64, Vec<Ghost>) {
         .iter()
         .fold((state.rand, vec![]), |(rand, ghosts), ghost| {
             let desired_vel = ghost.vel;
-            let cur_map_pos = state.pacman.pos / PX_PER_CELL as i32;
-            let on_square = (state.pacman.pos % PX_PER_CELL as i32).len_sq() == 0;
+            let cur_map_pos = ghost.pos / PX_PER_CELL as i32;
+            let on_square = (ghost.pos % PX_PER_CELL as i32).len_sq() == 0;
 
-            let all_dirs: Vec<Vec2d<i32>> = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-                .iter()
-                .map(|d| (*d).into())
-                .collect();
-            let available_dirs: Vec<Vec2d<i32>> = all_dirs.into_iter().fold(vec![], |acc, dir| {
-                let is_forward = ghost.vel == dir;
-                let is_backward = ghost.vel * -1 == dir;
-                let is_blocked = map_hit(&state.map, cur_map_pos + dir) == MapCell::Wall;
-                if !is_forward && !is_backward && !is_blocked {
-                    acc.into_iter().chain(once(dir)).collect()
-                } else {
-                    acc
-                }
-            });
             let rand = SmallRng::seed_from_u64(rand).next_u64();
-            let idx = rand as usize % available_dirs.len();
-            let next_dir = if on_square {
-                available_dirs[idx]
+            let new_vel = if on_square
+                && map_hit(&state.map, cur_map_pos + desired_vel) == MapCell::Wall
+            {
+                let all_dirs: Vec<Vec2d<i32>> = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+                    .iter()
+                    .map(|d| (*d).into())
+                    .collect();
+                let available_dirs: Vec<Vec2d<i32>> =
+                    all_dirs.into_iter().fold(vec![], |acc, dir| {
+                        let is_forward = ghost.vel == dir;
+                        let is_backward = ghost.vel * -1 == dir;
+                        let is_blocked = map_hit(&state.map, cur_map_pos + dir) == MapCell::Wall;
+                        if is_forward || is_backward || is_blocked {
+                            acc
+                        } else {
+                            acc.into_iter().chain(once(dir)).collect()
+                        }
+                    });
+                let idx = rand as usize % available_dirs.len();
+                let next_dir = available_dirs[idx];
+                next_dir
             } else {
-                ghost.vel
+                desired_vel
             };
-            let new_vel =
-                if on_square && map_hit(&state.map, cur_map_pos + desired_vel) == MapCell::Wall {
-                    next_dir
-                } else {
-                    desired_vel
-                };
 
             let new_pos = ghost.pos + new_vel;
 
             let ghost = Ghost {
                 pos: new_pos,
-                vel: desired_vel,
+                vel: new_vel,
             };
             let ghosts = ghosts.into_iter().chain(once(ghost)).collect();
             (rand, ghosts)
